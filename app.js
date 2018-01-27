87c860c08f32b08f5716b809d3d2e185ce53cccd
bit test <bit_test@Carleton.ca>
2018-01-26 20:40:21 -0500

    adding node dust starting point

--- app.js
+++ app.js
@@ -1,1 +1,26 @@
-console.log('hi'); 
+var express = require('express'),
+    path = require('path'),
+    bodyParser = require('body-parser'),
+    cons = require('consolidate'),
+    dust = require('dustjs-helpers'),
+    app = express();
+
+app.engine('dust', cons.dust);
+
+
+app.set('view engine', 'dust');
+app.set('views', __dirname + '/views');
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false}));
+
+app.get('/', function (req, res) {
+  res.render('index');
+});
+
+app.listen(3000, function () {
+  console.log('Server Started On Port 3000');
+
+});
---
+++ public/js/main.js
@@ -0,0 +1,8 @@
+$(document).ready(function () {
+  console.log('hello world');
+  console.log('hello world');
+  console.log('hello world');
+  console.log('hello world');
+  console.log('hello world');
+  console.log('hello world');
+});
